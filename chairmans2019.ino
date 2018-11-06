e5a481f6dd0739ca2e63f74f92280dab7cd7a012
EganJ <34986820+EganJ@users.noreply.github.com>
2018-11-06 11:30:20 -0600

    Added more utilities, filled in body of increment
    switched variable names to work (the variable is called stage), removed erroneous calls to modify stage. Added the LED fade effect (the method works, tried it on the donation box the other day) to Increment.

--- chairmans2019.ino
+++ chairmans2019.ino
@@ -33,10 +33,13 @@
 
 //Leds
 const int nLEDS=100;//How many LED's we have
-const int segmentSize=20;//How many LED's are in each gear
+const int segmentSize=20;//How many LED's are in each gear/segment
+const int nSegments=5;//How many segments are in the whole thing
 CRGB leds[nLEDS];
 const int LED_PIN = 41;
 CRGB current=CRGB(230,20,20);
+CRGB past=CRGB(255,255,255);
+CRGB future=CRGB(0,0,0);
 
 //Trellis
 const int TRELLIS_INT_PIN = A3;
@@ -79,18 +82,16 @@ void loop(){
     if(!(button==-1)){
         if(button<8){
             increment(1);
-            step++;
         }
         if(button==12){
             increment(-1);
-            step--;
         }
         if(button==15){
-            increment(-step);   //this should work, right? -mateo
-            step = 0;
+            increment(-stage);   //this should work, right? -mateo
+                                //Yes, but its called stage and is changed in-methods - Egan
         }
     }
-        trellis.readSwitches();//Reset the state of the trellis
+        trellis.readSwitches();//Reset the state of the trellis. Am i doing trellis right? -Egan
 }
 /**
 * Moves the stage by i. if I is one it will move forward one stage (one rotation, one led group).
@@ -100,14 +101,36 @@ void loop(){
 * to ensure that differences in setting LED's does not change the distance rotated.
 */
 void increment(int i){
+    stage+=i;
+    CRGB originalValues[nSegments];
+    CRGB endValues[nSegments];
+    for(int seg=0;seg<nSegments;seg++){
+      originalValues[seg]=getSegment(seg);
+      if(seg<stage){
+        endValues[seg]=past;
+      }
+      else if(seg==stage){
+        endValues[seg]=current;
+      }
+      else{
+        endValues[seg]=future;
+      }
+    }
     int startTime=micros();
     if(i>0){
         //set motor forward
     }else{
         //set motor backward
     }
-    while(micros()<startTime+abs(i)*timeInRotation){
-
+    int duration=abs(i)*timeInRotation
+    while(micros()<startTime+duration){
+      t=micros();//Initialize time for uniform brighness
+      for(int seg=0;seg<nSegments;seg++){
+        //set each to the blend of its original and end values with the more time giving more weight to the end color.
+        //The abs() is there to ensure that the delay in the two micros() calls above does not cause a negative value.
+        setSegment(blendColor(endValues[seg],originalValues[seg],t,abs(startTime+duration-startTime));
+      }
+      FastLED.show()
     }
     //set motor 0
 }
@@ -126,7 +149,34 @@ void setSegment(int i,CRGB c){
         leds[ind]=c;
     }
 }
-
+void setSegment(int i,CRGB c,CRGB destination[]){
+    for(int ind=i*segmentSize;ind<i*segmentSize+segmentSize;i++){
+        destination[ind]=c;
+    }
+}
+/**
+ * Gets the value for a segment
+ */
+CRGB getSegment(int i){
+  return leds[i*segmentSize];
+}
+CRGB getSegment(int i, CRGB source[]){
+  return source[i*segmentSize];
+}
+/**
+ * Finds the blend of colors c1 and c2. The ratio of c1 to c2 is determined by alpha1 to alpha2.
+ * Alpha 1 and Alpha2 must be positive, but do not have to have a certain sum.
+ */
+CRGB blendColors(CRGB c1, CRGB c2, double alpha1, double alpha2){
+  double sum=alpha1+alpha2;
+  alpha1=alpha1/sum;
+  alpha2=alpha2/sum;
+  CRGB combined=CRGB(0,0,0);
+  for(int i=0;i<3;i++){
+    combined[i]=(byte)(c1[i]*alpha1+c2[i]*alpha2);
+  }
+ return combined; 
+}
 //Trellis utilities
 /**
  * Boot LEDs on trelis
