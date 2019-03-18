2711a30c4bc7dfd494e09431edd113e2848a5803
Mateo Silver <mateonsilver@gmail.com>
2019-03-17 20:07:17 -0500

    Commented

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -1,17 +1,16 @@
 #include <Servo.h>
 
-int analogPin = A7; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +5V
-int val = 0;  // variable to store the value read
-  Servo cim;
-  const int CIM_PIN = 9;
+int analogPin = A7;
+int val = 0;
+Servo cim;
+const int CIM_PIN = 9;
 int i = 0;
 
 void setup() {
   Serial.begin(9600);           //  setup serial
-  pinMode(analogPin, INPUT);
+  pinMode(analogPin, INPUT);    //  Set A7 as an input
   cim.attach(CIM_PIN);
-  cim.write(59);
+  cim.write(59);                //  Run motor at 1/3 speed backwards
   Serial.println("Start");
   delay(2000);
 }
@@ -21,20 +20,20 @@ void loop() {
   prev = curr;
   curr = analogRead(analogPin);
   Serial.println(i);
-//  val = analogRead(analogPin);  // read the input pin
-//  Serial.println(val);          // debug value
-if(curr > 500){
-  i++;
-  delay(20);
-//  Serial.println(i);          
-}
-if(i > 174){
-  i = 0;
-  cim.write(90);
-  Serial.println("Stop");
-  delay(4000);
-} else {
-  cim.write(59);
-}
-//delay(200);
+  //  val = analogRead(analogPin);  // read the input pin
+  //  Serial.println(val);          // debug value
+  if (curr > 500) {
+    i++;
+    delay(20);
+    //  Serial.println(i);
+  }
+  if (i > 174) {
+    i = 0;
+    cim.write(90);
+    Serial.println("Stop");
+    delay(4000);
+  } else {
+    cim.write(59);
+  }
+  //delay(200);
 }
