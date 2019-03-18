b9b4c4e3cec236d20c825a600e3fc723f1a190f6
Mateo Silver <mateonsilver@gmail.com>
2019-03-17 20:45:05 -0500

    Implementing method to find signal peaks

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -1,39 +1,42 @@
 #include <Servo.h>
 
-int analogPin = A7;
-int val = 0;
 Servo cim;
+
+int ANALOG_PIN = A7;
 const int CIM_PIN = 9;
+
+boolean isHigh = false;
+int curr = 0;
 int i = 0;
 
 void setup() {
   Serial.begin(9600);           //  setup serial
-  pinMode(analogPin, INPUT);    //  Set A7 as an input
+  pinMode(ANALOG_PIN, INPUT);   //  Set A7 as an input
   cim.attach(CIM_PIN);
-  cim.write(59);                //  Run motor at 1/3 speed backwards
+  cim.write(45);                //  Run motor at 1/2 speed backwards
   Serial.println("Start");
   delay(2000);
 }
-int prev = 0;
-int curr = 0;
+
 void loop() {
-  prev = curr;
-  curr = analogRead(analogPin);
-  Serial.println(i);
-  //  val = analogRead(analogPin);  // read the input pin
-  //  Serial.println(val);          // debug value
-  if (curr > 500) {
-    i++;
-    delay(20);
-    //  Serial.println(i);
+  curr = analogRead(ANALOG_PIN);
+  Serial.print("Current: ");
+  Serial.println(curr);
+
+  if (curr > 400) {             //  If the current value is greater than 400 (peak), set isHigh to true
+    isHigh = true;
   }
-  if (i > 174) {
+  if (isHigh && curr < 200) {   //  If there's a peak and the current value is below 200 (trough), iterate and reset isHigh to false
+    i++;
+    isHigh = false;
+  }
+
+  if (i > 174) {                //  Stop rotating after one revolution
     i = 0;
     cim.write(90);
     Serial.println("Stop");
     delay(4000);
   } else {
-    cim.write(59);
+    cim.write(45);              //  Keep rotating if you haven't finished one revolution
   }
-  //delay(200);
 }
