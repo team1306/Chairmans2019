d5ba6070f608d8d7846fdda070b6ee18b05d2b8b
Mateo Silver <mateonsilver@gmail.com>
2019-03-20 08:42:55 -0500

    Documentation

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -2,18 +2,22 @@
 
 Servo cim;
 
-int ANALOG_PIN = A7;
+int ANALOG_PIN_NEGATIVE = A7;
+int ANALOG_PIN_POSITIVE = A5;
 const int CIM_PIN = 9;
 
 boolean isHigh = false;
-int curr = 0;
+
+int currPositive = 0;
+int currNegative = 0;
 int i = 0;
 
 void setup() {
   Serial.begin(9600);           //  setup serial
-  pinMode(ANALOG_PIN, INPUT);   //  Set A7 as an input
+  pinMode(ANALOG_PIN_NEGATIVE, INPUT);   //  Set A7 as an input
+  pinMode(ANALOG_PIN_POSITIVE, INPUT);   //  Set A5 as an input
   cim.attach(CIM_PIN);
-  cim.write(45);                //  Run motor at 1/2 speed backwards
+  cim.write(180);                //  Run motor at full speed
   Serial.println("Start");
   delay(2000);
 }
@@ -21,28 +25,36 @@ void setup() {
 void loop() {
   int numRotations = 1;
   rotate(numRotations);
+//    cim.write(180);
 }
 
 void rotate(int r){
-  curr = analogRead(ANALOG_PIN);
-  Serial.print("Current: ");
-  Serial.println(curr);
-
-  if (curr > 400) {             //  If the current value is greater than 400 (peak), set isHigh to true
+  currPositive = analogRead(ANALOG_PIN_POSITIVE);
+  currNegative = analogRead(ANALOG_PIN_NEGATIVE);
+  Serial.print("Current+: ");
+  Serial.println(currPositive);
+  
+  Serial.print("Current-: ");
+  Serial.println(currNegative);
+  if (currPositive > 475) {             //  If the current value is greater than 475 (peak), set isHigh to true
     isHigh = true;
+    Serial.println("isHigh: ");
+    Serial.println(isHigh);
   }
-  if (isHigh && curr < 200) {   //  If there's a peak and the current value is below 200 (trough), iterate and reset isHigh to false
+  if (isHigh && currNegative < 375) {   //  If there's a peak and the current value is below 375 (trough), iterate and reset isHigh to false
     i++;
+    Serial.print("i ");
+    Serial.println(i);
     isHigh = false;
   }
 
-  if (i > 174*r) {                //  Stop rotating after one revolution
+  if (i >= 174*r) {                //  Stop rotating after one revolution
     i = 0;
     cim.write(90);
     Serial.println("Stop");
     delay(4000);
   } else {
-    cim.write(45);              //  Keep rotating if you haven't finished one revolution
+    cim.write(180);              //  Keep rotating if you haven't finished one revolution
   }
 
 }
