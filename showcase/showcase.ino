2214e274e7aff6abfbf7776f1b8be69660582cb9
Mateo Silver <mateonsilver@gmail.com>
2019-04-09 23:23:57 -0500

    Showcase framework

--- showcase/showcase.ino
+++ showcase/showcase.ino
@@ -1,17 +1,67 @@
 #include <FastLED.h>
 #include <Servo.h>
 
+//LEDs
+const int segmentSize = 33; // How many LED's are in each gear/segment
+const int nSegments = 5;    // How many segments are in the whole thing
+const int nLEDS = segmentSize * nSegments;   // How many LED's we have
+const int LED_PIN = 42;
+CRGB leds[nLEDS];
+
+//SWITCH
+const int LIMIT_SWITCH_PIN = 2;
+
+//MOTOR
+Servo cim;
+const int CIM_PIN = 9;
+
+//EFFECTS
+int effectNum = 10;
+
 void setup() {
-  // put your setup code here, to run once:
-  int limitSwitchPin = 2;
-  pinMode(limitSwitchPin, INPUT); 
+  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, nLEDS);
+
+  pinMode(LIMIT_SWITCH_PIN, INPUT);
+
+  Serial.println("Attaching CIM as Servo");
+  cim.attach(CIM_PIN);
+  cim.write(90);                //  Stop motor to start
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(limitSwitchPin) == LOW){
-      cim.write(180);
-      delay(1000);
-      cim.write(90);
+  if (digitalRead(LIMIT_SWITCH_PIN) == LOW) {
+    cim.write(180);
+    delay(1000);
+    cim.write(90);
   }
+
+  for (int i = 0; i < effectNum; i++) {
+    effect(effectNum);
+  }
+}
+
+void effect(int effectNum) {
+  switch (effectNum) {
+    case 0:
+      allColor(255, 0, 0);
+      break;
+    case 1:
+      allColor(0, 255, 0);
+      break;
+    case 2:
+      allColor(0, 0, 255);
+      break;
+    default:
+      break;
+  }
+}
+
+void allColor(int r, int g, int b) {
+  for (int i = 0; i < nLEDS; i++) {
+    leds[i].red   = r;
+    leds[i].green = g;
+    leds[i].blue  = b;
+  }
+  FastLED.show();
 }
